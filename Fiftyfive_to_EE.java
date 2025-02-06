ee78a7207d0686f8e08a206828efe56a17d6c383
darshan9552 <117584150+darshan9552@users.noreply.github.com>
2025-02-07 00:43:30 +0530

    Add files via upload

---
+++ Fiftyfive_to_EE.java
@@ -0,0 +1,12 @@
+package my_batch_1233;
+
+public class Fiftyfive_to_EE {
+	    public static void main(String[] args) {
+	        for (int i = 55; i<= 88; i+= 3) {
+	            System.out.println(i);
+	        }
+	    }
+	}
+
+
+
---
+++ eleven_to_MTT.java
@@ -0,0 +1,10 @@
+package my_batch_1233;
+
+public class eleven_to_MTT {
+    public static void main(String[] args) {
+        for (int i = 11; i>= -33; i-= 4) {
+            System.out.println(i);
+        }
+    }
+}
+
