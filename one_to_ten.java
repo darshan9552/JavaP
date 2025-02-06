992127b146f28c9da9b95f5fdcb92c5b1b2c2d66
darshan9552 <117584150+darshan9552@users.noreply.github.com>
2025-02-07 00:01:21 +0530

    Add files via upload

---
+++ one_to_ten.java
@@ -0,0 +1,15 @@
+package my_batch_1233;
+
+public class one_to_ten {
+	public static  void main(String[] args) {
+		
+		for(int k=1; k<=10; k++)
+		{
+		System.out.println(k);
+		}
+	
+	}
+
+
+
+}
---
+++ while_loop.java
@@ -0,0 +1,14 @@
+package my_batch_1233;
+
+public class while_loop {
+	public static void main(String[] args) {
+		
+		int k=1;
+		while(k<=10) 
+		{
+			System.out.println(k);
+			k++;
+		}
+	}
+
+}
